c4e1aa50b1e0bf63585a992ac70c86cbf0f4125e
Brian Parker <btparker70@gmail.com>
2022-02-23 08:27:26 -0800

    finished filecreator

---
+++ fileCreator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#############################################
+# v1.0 of script by Brian Parker
+# date 2022/2/23
+#############################################
+
+# writing a script to generate 15 files
+
+for i in {1..15}
+do
+  touch brianparker-scripting-activity$i.txt
+done
